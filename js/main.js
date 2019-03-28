7ea148acabc548bece87888648662f576626ae84
KhanyisoHaman <khanyisohaman@gmail.com>
2019-03-28 16:16:02 +0200

    edit shop page

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ $(window).scroll(function() {
     btn.removeClass('show');
   }
 });
+
+// function changeColor() {
+//   let btn = document.getElementById("heart");
+//       btn1 = document.getElementById("heart-1");
+//       btn2 = document.getElementById("heart-2");
+//       btn3 = document.getElementById("heart-3");
+
+//   btn.style.color = "red";
+//   btn1.style.color = "red";
+//   btn2.style.color = "red";
+//   btn3.style.color = "red";
+// }
